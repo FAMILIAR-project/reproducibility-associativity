dba4185638acf2fb873a436ff8c21b401a411110
Mathieu Acher <mathieu.acher@irisa.fr>
2023-03-01 17:27:53 +0100

    testassoc.py can be parameterized through command line eg python testassoc.py --seed 1234 --number 1000

--- testassoc.py
+++ testassoc.py
@@ -1,12 +1,22 @@
+import argparse
 from random import random, seed
+
 def associativity_test() -> bool:
     x = random(); y = random(); z = random()
     return x+(y+z) == (x+y)+z
-def proportion(number: int) -> int:
+
+def proportion(number: int, seed_val: int) -> int:
+    seed(seed_val)
     ok = 0
     for i in range(number):
         ok += associativity_test()
     return ok*100/number
 
-seed(int(input('Seed: ')))
-print(str(proportion(10000))+"%")
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Associativity test with seed.')
+    parser.add_argument('--seed', type=int, required=True, help='Seed value')
+    parser.add_argument('--number', type=int, default=10000, help='Number of tests')
+    args = parser.parse_args()
+
+    print(str(proportion(args.number, args.seed))+"%")
+
